4da7f1e0cfb790df28a3d82c7f29c368ba76a8cb
Adam <adam.toth.n@gmail.com>
2017-05-05 23:56:20 +0100

    Update interactive.js

--- js/interactive.js
+++ js/interactive.js
@@ -1,74 +1,83 @@
-$(document).ready(function() {    
+$(document).ready(function() {  
   var randomizeQuotes;
   var quotes;
   var randomBackgroundColors;
-  var randomize;   
-  
-  document.getElementById("message").innerHTML = "<h2>Anyone who has never made a mistake has never tried anything new.</h2> </br> - </br><p>Albert Einstein</p>";
-  
-  $("button").click(function() {     
-    //9 random quotes to match the 9 colors
+  var randomize;
+
+  function obtainQuote()  {
+    //9 quotes to match the 9 colors
     quotes = [
       "<h2>Anyone who has never made a mistake has never tried anything new.</h2> </br> - </br><p>Albert Einstein</p>",
       "<h2>Life is like riding a bicycle. To keep your balance, you must keep moving.</h2> </br> - </br><p>Albert Einstein</p>",
       "<h2>The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts.</h2> </br> - </br><p>Bertrand Russell</p>",
-      "<h2>When one person suffers from a delusion it is called insanity; when many people suffer from a delusion it is called religion.</h2> </br> - </br><p>Robert Pirsig</p>",
+      "<h2>When one person suffers from a delusion it is called insanity. When many people suffer from a delusion it is called religion.</h2> </br> - </br><p>Robert Pirsig</p>",
       "<h2>I find that the harder I work, the more luck I seem to have.</h2> </br> - </br><p>Thomas Jefferson</p>",
       "<h2>Do, or do not. There is no 'try'.</h2> </br> - </br><p>Yoda</p>",
       "<h2>The only way to get rid of a temptation is to yield to it.</h2> </br> - </br><p>Oscar Wilde</p>",
       "<h2>Don't stay in bed, unless you can make money in bed.</h2> </br> - </br><p>George Burns</p>",
       "<h2>If you can't get rid of the skeleton in your closet, you'd best teach it to dance.</h2> </br> - </br><p>George Bernard Shaw</p>"
     ];
-        
+   
+    //9 random colors for the 9 quotes
+    randomBackgroundColors = ["#4286f4", "#214c91",
+                              "#4292af", "#40ad8c",
+                              "#ad4040", "#db2727",
+                              "#3d1426", "#993762",
+                              "#613799"];
+
     randomizeQuotes = quotes[Math.floor(Math.random() * quotes.length)];
-    
-    //9 random colors for the 9 random quotes    
-    randomBackgroundColors = ["#4286f4", "#214c91", 
-                                  "#4292af", "#40ad8c", 
-                                  "#ad4040", "#db2727", 
-                                  "#3d1426", "#993762", 
-                                  "#613799"];     
-    randomize = randomBackgroundColors[Math.floor(Math.random() * randomBackgroundColors.length)];
-        
-    $("body").css({"background": randomize});   
-    $(".color-paragraph").css({"color": randomize});
-    $("#button-text").css({"color": randomize});
-    $(".btn-default").css({"border-color": randomize});     
+    randomizeColors = randomBackgroundColors[Math.floor(Math.random() * randomBackgroundColors.length)];    
+      
+    $("body").css({"background": randomizeColors});
+    $(".color-paragraph").css({"color": randomizeColors});
+    $("#button-text").css({"color": randomizeColors});
+    $(".btn-default").css({"border-color": randomizeColors});
     $("#message").html("<p>" + randomizeQuotes + "</p>");
-    $("#message p").css({"color": randomize, "font-weight": 800});
-    $("#message h2").css({"color": randomize});    
-  })
+    $("#message p").css({"color": randomizeColors, "font-weight": 800});
+    $("#message h2").css({"color": randomizeColors});
+   }
+  
+    obtainQuote();
+  
+    //100% reproducing the above code (temporary solution, but for now the only one that works)
+    $("button").click(function() {
+      quotes = [
+        "<h2>Anyone who has never made a mistake has never tried anything new.</h2> </br> - </br><p>Albert Einstein</p>",
+        "<h2>Life is like riding a bicycle. To keep your balance, you must keep moving.</h2> </br> - </br><p>Albert Einstein</p>",
+        "<h2>The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts.</h2> </br> - </br><p>Bertrand Russell</p>",
+        "<h2>When one person suffers from a delusion it is called insanity; when many people suffer from a delusion it is called religion.</h2> </br> - </br><p>Robert Pirsig</p>",
+        "<h2>I find that the harder I work, the more luck I seem to have.</h2> </br> - </br><p>Thomas Jefferson</p>",
+        "<h2>Do, or do not. There is no 'try'.</h2> </br> - </br><p>Yoda</p>",
+        "<h2>The only way to get rid of a temptation is to yield to it.</h2> </br> - </br><p>Oscar Wilde</p>",
+        "<h2>Don't stay in bed, unless you can make money in bed.</h2> </br> - </br><p>George Burns</p>",
+        "<h2>If you can't get rid of the skeleton in your closet, you'd best teach it to dance.</h2> </br> - </br><p>George Bernard Shaw</p>"
+      ];
+     
+      randomBackgroundColors = ["#4286f4", "#214c91",
+                                "#4292af", "#40ad8c",
+                                "#ad4040", "#db2727",
+                                "#3d1426", "#993762",
+                                "#613799"];
+  
+      randomizeQuotes = quotes[Math.floor(Math.random() * quotes.length)];
+      randomizeColors = randomBackgroundColors[Math.floor(Math.random() * randomBackgroundColors.length)];   
+      
+      $("body").css({"background": randomizeColors});
+      $(".color-paragraph").css({"color": randomizeColors});
+      $("#button-text").css({"color": randomizeColors});
+      $(".btn-default").css({"border-color": randomizeColors});
+      $("#message").html("<p>" + randomizeQuotes + "</p>");
+      $("#message p").css({"color": randomizeColors, "font-weight": 800});
+      $("#message h2").css({"color": randomizeColors});
+    }); 
+  
+    $('#tweet-quote').click(function() {        
+      function openURL(noBracketsMessage){
+        window.open(noBracketsMessage, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+      }
+      var tweetQuote = $("#message").text();
+      if (tweetQuote != 'Press below for a quote') {
+        window.open('https://twitter.com/intent/tweet?text="' + tweetQuote + '"', '_blank');
+      }
+    })
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
